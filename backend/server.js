d0d564a7de46b10459b551f3609ac11530024e6a
Kavishna Thathsara <kavishna@Kavishnas-MacBook-Pro.local>
2023-06-19 12:04:04 +0530

    app connected to the database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const adRoutes = require("./routes/routes");
 
 const app = express();
 app.use(express.json());
 
-//routes
-app.use("/api/ads", adRoutes);
-
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+// Set up MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
+
+    console.log("connected to database");
+  })
+  .catch((error) => {
+    console.error("error connecting to database", error);
+  });
+
+//routes
+app.use("/api/ads", adRoutes);
